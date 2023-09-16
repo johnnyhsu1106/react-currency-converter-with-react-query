c6186a1f4837bd1a5462833ef7c2ce235e7698fe
Johnny Hsu <yuwei.hsu@dowjones.com>
2023-09-16 16:02:07 -0500

    update variable name: context -> value

--- src/context/CurrencyContext.jsx
+++ src/context/CurrencyContext.jsx
@@ -89,7 +89,7 @@ const CurrencyProvider = ({ children }) => {
     setAmountInFromCurrency(false);
   };
   
-  const currencyValue = {
+  const value = {
     isLoading,
     isError,
     currencyOptions,
@@ -105,7 +105,7 @@ const CurrencyProvider = ({ children }) => {
 
 
   return (
-    <CurrencyContext.Provider value={currencyValue}>
+    <CurrencyContext.Provider value={value}>
       {children}
     </CurrencyContext.Provider> 
   )
